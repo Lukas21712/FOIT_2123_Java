aa1be50260253907a40b8baf02438f8173643be6
Lukas Dombert <82817950+Lukas21712@users.noreply.github.com>
2021-12-07 09:34:33 +0100

    Initial commit

--- src/v_schleifen/v_xxxx/Galgenmaenchen.java
+++ src/v_schleifen/v_xxxx/Galgenmaenchen.java
@@ -1,4 +1,22 @@
 package v_schleifen.v_xxxx;
 
+import java.util.Scanner;
+
 public class Galgenmaenchen {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String word = "test";
+        String eingabe = null;
+        int length = word.length();
+
+        for (int counter = 0; counter != length; counter ++) {
+            System.out.print("_ ");
+        }
+
+        while (word != eingabe) {
+            System.out.println("Bitte geben Sie einen Buchstaben ein: ");
+            eingabe = input.next();
+        }
+    }
 }
