67a8015c70bf06f42f387498495589d3f06a95d1
Lukas Dombert <82817950+Lukas21712@users.noreply.github.com>
2022-05-24 09:32:05 +0200

    Initial commit

--- src/StringArray.java
+++ src/StringArray.java
@@ -22,11 +22,43 @@ public class StringArray {
         System.out.println("3: Länge des Satzes");
         System.out.println("4: Alles klein schreiben");
         System.out.println("5: Alles groß schreiben");
-        System.out.println("6: Wörter trennen");
-        System.out.println("7: Gibt den Index ");
-        System.out.println("8: equals");
-        System.out.println("9: LastIndexof");
+        System.out.println("6: split");
+        System.out.println("7: indexOf");
+        System.out.println("8: lastIndexOf");
+        System.out.println("9: equals");
         System.out.println("Bitte geben Sie einen Index ein:");
         auswahl = input.nextInt();
+
+        switch (auswahl) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                for (int i = 0; i < anzahl; i++) {
+                    System.out.println(StringCases.get(i).length());
+                }
+                break;
+            case 4:
+                for (int i = 0; i < anzahl; i++) {
+                    System.out.println(StringCases.get(i).toLowerCase());
+                }
+                break;
+            case 5:
+                for (int i = 0; i < anzahl; i++) {
+                    System.out.println(StringCases.get(i).toUpperCase());
+                }
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+            case 9:
+                break;
+            default:
+                System.out.println("Dies ist kein gültiger Parameter");
+        }
     }
 }
